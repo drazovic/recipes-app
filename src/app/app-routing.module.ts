e6a9030b399f87f7d74e71fdcdaa2fccd81c1739
drazovic <idrazovic@gmail.com>
2020-12-18 00:41:25 +0100

    app routing module updated with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    {
+        path: 'recipes',
+        component: RecipesComponent,
+        children: [
+            {
+                path: '',
+                component: RecipeStartComponent,
+            },
+            {
+                path: ':id',
+                component: RecipeDetailComponent,
+            },
+        ],
+    },
+    { path: 'shopping-list', component: ShoppingListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

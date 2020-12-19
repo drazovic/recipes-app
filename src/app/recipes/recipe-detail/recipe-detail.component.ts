515c088e2566f8392a316818ae7ba2f9702c3735
drazovic <idrazovic@gmail.com>
2020-12-19 19:51:09 +0100

    data added

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.scss']
+    selector: 'app-recipe-detail',
+    templateUrl: './recipe-detail.component.html',
+    styleUrls: ['./recipe-detail.component.scss'],
 })
 export class RecipeDetailComponent implements OnInit {
+    @Input() recipe: Recipe;
 
-  constructor() { }
+    constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    onAddToShoppingList() {
+        this.shoppingListService.addIngredients(this.recipe.ingredients);
+    }
 }

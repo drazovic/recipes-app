49af53ba1ba15f01653a0d3da0022a409eb69795
drazovic <idrazovic@gmail.com>
2020-12-19 20:37:27 +0100

    recipe edit component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
     declarations: [
@@ -26,6 +27,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
         ShoppingEditComponent,
         DropdownDirective,
         RecipeStartComponent,
+        RecipeEditComponent,
     ],
     imports: [BrowserModule, AppRoutingModule, FormsModule],
     providers: [],

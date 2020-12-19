1e6f1fe4e4b51ad17f89b06a72a55f71df6713bc
drazovic <idrazovic@gmail.com>
2020-12-19 19:50:35 +0100

    data flow implemented

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+    selector: 'app-recipes',
+    templateUrl: './recipes.component.html',
+    styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent implements OnInit {
+    selectedRecipe: Recipe;
 
-  constructor() { }
+    constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+            this.selectedRecipe = recipe;
+        });
+    }
 }

3edba4095760d306544d48491bda71483717d078
drazovic <idrazovic@gmail.com>
2020-12-19 19:53:15 +0100

    shopping list service implemented

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
     selector: 'app-shopping-list',
@@ -7,12 +8,16 @@ import { Ingredient } from '../shared/ingredient.model';
     styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
-    ingredients: Ingredient[] = [
-        new Ingredient('eggs', 3),
-        new Ingredient('apples', 4),
-    ];
+    ingredients: Ingredient[] = [];
 
-    constructor() {}
+    constructor(private shoppingListService: ShoppingListService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.ingredients = this.shoppingListService.getIngeredients();
+        this.shoppingListService.ingredientsChanged.subscribe(
+            (ingredients: Ingredient[]) => {
+                this.ingredients = ingredients;
+            }
+        );
+    }
 }

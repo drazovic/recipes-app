3c264704ce12a5e0890307875ddbee307533513e
drazovic <idrazovic@gmail.com>
2020-12-19 19:52:24 +0100

    data added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+    selector: 'app-shopping-edit',
+    templateUrl: './shopping-edit.component.html',
+    styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent implements OnInit {
+    @ViewChild('nameInput') nameInputRef: ElementRef;
+    @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  constructor() { }
+    constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    onAddItem() {
+        const ingredientName = this.nameInputRef.nativeElement.value;
+        const ingredientAmount = this.amountInputRef.nativeElement.value;
+        const ingredient = new Ingredient(ingredientName, ingredientAmount);
+        this.shoppingListService.addIngredient(ingredient);
+    }
 }
